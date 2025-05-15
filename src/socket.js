48337320488c40ee5375461936017b1f4f73fb30
DEV-endra <devendra_2301cs91@iitp.ac.in>
2025-05-15 05:55:33 +0000

    change host url

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("ws://localhost:3000", {
+const socket = io("wss://hopeconnect-backend.onrender.com", {
     transports: ["websocket"]
 });
 
