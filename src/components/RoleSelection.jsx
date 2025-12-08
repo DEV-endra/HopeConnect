7132d1b09128834ebf71a100fc8709f1cd4c841c
DEV-endra <devendra_2301cs91@iitp.ac.in>
2025-12-08 20:21:00 +0000

    fix import

--- src/components/RoleSelection.jsx
+++ src/components/RoleSelection.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import { useNavigate } from "react-router-dom"; // Importing navigation hook
 import styles from "../styles/RoleSelection.module.css"; // Importing CSS module
 import Logo from "../assets/Logo.png";
